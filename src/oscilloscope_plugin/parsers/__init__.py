280557b63185ab801861dff6f8f7daf1bb25ba83
Paul Levasseur <paul.levasseur@epita.fr>
2024-11-13 11:12:15 +0100

    mainfile re remove

--- src/oscilloscope_plugin/parsers/__init__.py
+++ src/oscilloscope_plugin/parsers/__init__.py
@@ -14,5 +14,4 @@ class NewParserEntryPoint(ParserEntryPoint):
 parser_entry_point = NewParserEntryPoint(
     name='NewParser',
     description='New parser entry point configuration.',
-    mainfile_name_re='.*\.newmainfilename',
 )
